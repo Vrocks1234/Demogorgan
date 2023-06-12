4382c46b8cfc1f337d38ef4afc9c362d4ee8dce6
Vrocks1234 <136353261+Vrocks1234@users.noreply.github.com>
2023-06-12 11:16:55 -0500

    Update append.py

--- ass 1/append.py
+++ ass 1/append.py
@@ -1,1 +1,7 @@
-
+List1 = ["William","Michael","Emma","David"]
+List2 = ["Taylor","Anderson","Martin","Miller"]
+list3 = []
+for x,y in zip(list1,list2):
+  c = x+" "+Y
+  list3.append(c)
+print(list3)
